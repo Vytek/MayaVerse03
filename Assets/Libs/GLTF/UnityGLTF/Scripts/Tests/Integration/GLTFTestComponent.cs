59fc577e9bb1dd3c945252fdcca6ba9c054cdc3f
Enrico Speranza <enrico.speranza@gmail.com>
2018-07-04 18:21:18 +0200

    Add latest UnityGLTF release

---
+++ Assets/Libs/GLTF/UnityGLTF/Scripts/Tests/Integration/GLTFTestComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityGLTF.Loader;
+
+namespace UnityGLTF.Tests.Integration
+{
+	public class GLTFTestComponent : MonoBehaviour
+	{
+		public string Url;
+		public bool Multithreaded = true;
+
+
+		IEnumerator Start()
+		{
+			ILoader loader = new WebRequestLoader(URIHelper.GetDirectoryName(Url));
+			var sceneImporter = new GLTFSceneImporter(
+				URIHelper.GetFileFromUri(new Uri(Url)),
+				loader
+				);
+
+			sceneImporter.SceneParent = gameObject.transform;
+			yield return sceneImporter.LoadScene(-1, Multithreaded);
+			IntegrationTest.Pass();
+		}
+	}
+}
