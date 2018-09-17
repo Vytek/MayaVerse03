dbe8b5532e2e4766b1db5b34ddc3f89ce8bd2c22
Enrico Speranza <enrico.speranza@gmail.com>
2018-09-17 22:18:40 +0200

    Initial stub for rez command.

---
+++ Assets/Scripts/CommandRezScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using CommandTerminal;
+
+public static class RezCommand
+{
+    [RegisterCommand(Help = "Rez Object in MayaVerse")]
+    static void CommandRez(CommandArg[] args)
+    {
+        Terminal.Log("Hello world!");
+    }
+}
