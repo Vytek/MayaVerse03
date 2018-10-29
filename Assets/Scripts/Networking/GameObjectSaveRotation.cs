24a7541e97c15ef8cdd8b20a250d6c95131086d4
Enrico Speranza <enrico.speranza@gmail.com>
2018-10-29 22:07:29 +0100

    Initial Config for SaveRotationPosition

---
+++ Assets/Scripts/Networking/GameObjectSaveRotation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BayatGames.SaveGameFree.Types;
+using BayatGames.SaveGameFree.Serializers;
+using BayatGames.SaveGameFree;
+
+public class GameObjectSaveRotation : MonoBehaviour {
+
+    public Transform target;
+    public bool loadOnStart = true;
+    public string identifier = "exampleSavePositionRotation.dat";
+
+    // Use this for initialization
+    void Start () {
+        SaveGame.Serializer = new SaveGameBinarySerializer();
+        if (loadOnStart)
+        {
+            Load();
+        }
+    }
+	
+    public void Save()
+    {
+        SaveGame.Save<Vector3Save>(identifier, target.position, SaveGamePath.DataPath);
+        SaveGame.Save<QuaternionSave>(identifier, target.rotation, SaveGamePath.DataPath);
+    }
+
+    public void Load()
+    {
+        target.position = SaveGame.Load<Vector3Save>(
+            identifier,
+            Vector3.zero,
+            SaveGamePath.DataPath);
+        target.rotation = SaveGame.Load<QuaternionSave>(
+                identifier,
+                Quaternion.identity,
+                SaveGamePath.DataPath);
+    }
+}
